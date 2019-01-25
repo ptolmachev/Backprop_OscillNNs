01e6148f74a146a1b20fe5299dbec7efe0b3c82e
Pavel <betadecay1993@gmail.com>
2019-01-25 20:06:26 +1100

    Add files via upload
    Uploaded to github

--- cont_backprop_in_oscillNN_with_TF.py
+++ cont_backprop_in_oscillNN_with_TF.py
@@ -2,17 +2,16 @@ import sympy as sym
 import numpy as np
 from matplotlib import pyplot as plt
 import pickle
-from collections import deque
 
 num_nrns = 2
 # SYMBOLIC VARIABLES
 x = sym.Symbol("x")
 V = sym.MatrixSymbol("V", 1, num_nrns)
+H = sym.MatrixSymbol("H", 1, num_nrns)
 M = sym.MatrixSymbol("M", 1, num_nrns)
 Z_inh = sym.MatrixSymbol("Z_inh", num_nrns, num_nrns*num_nrns) # V sensitivity to W_inh
 Y_inh = sym.MatrixSymbol("Y_inh", num_nrns, num_nrns*num_nrns) # M sensitivity to W_inh
 W_inh = sym.MatrixSymbol("W_inh", 1, num_nrns*num_nrns)
-H = sym.MatrixSymbol("H", 1, num_nrns)
 
 # MODEL PARAMETERS
 V_half = sym.Symbol("V_half")
@@ -52,21 +51,14 @@ for i in range(num_nrns):
     G[i] = (k_ad * (1 / (1 + sym.exp(-(V[0,i] - V_half) / k_v))) - M[0,i]) / tau_ad
 
 
-# #SUBSTITUTING V BY H
-# for i in range(num_nrns):
-#     F[i] = F[i].subs(V,H)
-#     G[i] = G[i].subs(V,H)
+#SUBSTITUTING V BY H
+for i in range(num_nrns):
+    F[i] = F[i].subs(V,H)
+    G[i] = G[i].subs(V,H)
 
 print("\nfunction F is defined : \n{}".format(F))
 print("\nfunction M is defined : \n{}".format(G))
 
-
-#DIFFERENTIATING F WRT V
-F_diff_v = np.empty((num_nrns, num_nrns), dtype=object)
-for i in range(num_nrns):
-    for j in range(num_nrns):
-        F_diff_v[i, j] = F[i].diff(V[0,j])
-print("\n the derivative Fv is  : \n{}".format(F_diff_v))
 
 #DIFFERENTIATING F WRT M
 F_diff_m = np.empty((num_nrns, num_nrns), dtype=object)
@@ -82,13 +74,6 @@ for i in range(num_nrns):
         F_diff_w[i, j] = F[i].diff(W_inh[0,j])
 print("\n the derivative of Fm is  : \n{}".format(F_diff_w))
 
-#DIFFERENTIATING G WRT V
-G_diff_v = np.empty((num_nrns, num_nrns), dtype=object)
-for i in range(num_nrns):
-    for j in range(num_nrns):
-        G_diff_v[i, j] = G[i].diff(V[0,j])
-print("\n the derivative of Gv is  : \n{}".format(G_diff_v))
-
 #DIFFERENTIATING G WRT M
 G_diff_m = np.empty((num_nrns, num_nrns), dtype=object)
 for i in range(num_nrns):
@@ -102,8 +87,8 @@ RHS_Y_inh = np.zeros((num_nrns,num_nrns*num_nrns), dtype=object)
 for i in range(num_nrns): # number of f rhs
     for j in range(num_nrns*num_nrns): #number of parameter wrt to differentiate j -> (k,n)
         for m in range(num_nrns):  # sweeping index
-            RHS_Z_inh[i, j] += F_diff_v[i, m] * Z_inh[m, j] + F_diff_m[i, m] * Y_inh[m, j]
-            RHS_Y_inh[i, j] += G_diff_v[i, m] * Z_inh[m, j] + G_diff_m[i, m] * Y_inh[m, j]
+            RHS_Z_inh[i, j] +=  F_diff_m[i, m] * Y_inh[m, j]
+            RHS_Y_inh[i, j] +=  G_diff_m[i, m] * Y_inh[m, j]
         RHS_Z_inh[i, j] += F_diff_w[i, j]
 
 # ERROR FUNCTION
@@ -120,16 +105,16 @@ for j in range(num_nrns*num_nrns):
 
 # CASTING SYMBOLIC FUNCTION INTO NUMPY (LAMBDIFY)
 for i in range(F.shape[0]):
-    F[i] = sym.lambdify((V,M,W_inh,V_half),F[i],"numpy")
-    G[i] = sym.lambdify((V,M,V_half),G[i],"numpy")
+    F[i] = sym.lambdify((H, M, W_inh, V_half),F[i],"numpy")
+    G[i] = sym.lambdify((H, M, V_half),G[i],"numpy")
 
 for i in range(RHS_Z_inh.shape[0]):
     for j in range(RHS_Z_inh.shape[1]):
-        RHS_Z_inh[i, j] = sym.lambdify((V, M, W_inh, Z_inh, Y_inh, V_half), RHS_Z_inh[i, j], "numpy")
+        RHS_Z_inh[i, j] = sym.lambdify((H, M, W_inh, Z_inh, Y_inh, V_half), RHS_Z_inh[i, j], "numpy")
 
 for i in range(RHS_Y_inh.shape[0]):
     for j in range(RHS_Y_inh.shape[1]):
-        RHS_Y_inh[i, j] = sym.lambdify((V, M, W_inh, Z_inh, Y_inh, V_half), RHS_Y_inh[i, j], "numpy")
+        RHS_Y_inh[i, j] = sym.lambdify((H, M, W_inh, Z_inh, Y_inh, V_half), RHS_Y_inh[i, j], "numpy")
 
 for j in range(RHS_W_inh.shape[0]):
     RHS_W_inh[j] = sym.lambdify((V, H, Z_inh, Y_inh), RHS_W_inh[j], "numpy")
@@ -144,7 +129,7 @@ M = np.random.rand(1,2)
 Z_inh = 0.0*np.ones((num_nrns, num_nrns*num_nrns)) ###!
 Y_inh = 0.0*np.ones((num_nrns, num_nrns*num_nrns)) ###!
 V_half = -30
-W_inh = np.array([[0, 0.35, 0.2, 0]])
+W_inh = np.array([[0.1, 0.1, 0.1, 0.1]])
 dt = 0.1
 vals_V = [[V[0,0]],[V[0,1]]]
 vals_M = [[M[0,0]],[M[0,1]]]
@@ -168,55 +153,45 @@ for i in range(vals_W_inh.shape[1]):
 
 t = [0]
 stoptime = t_target[-1]
-# for i in range(int(stoptime/dt)):
-#     if (int(i/dt)%int(0.1*stoptime/dt)== 0):
-#         print("\rSimulated {}%".format(np.ceil(i*100/int(stoptime/dt))))
-i = 0
-error = np.inf
-eps = 0.01
-errors = deque(maxlen = 100)
-while error > eps:
-    if (int(i / dt) % int(0.1 * stoptime / dt) == 0):
-        print("\rError = {}".format(error))
+for i in range(int(stoptime/dt)):
+
+    if (int(i/dt)%int(0.1*stoptime/dt)== 0):
+        print("\rSimulated {}%".format(np.ceil(i*100/int(stoptime/dt))))
+
+    H = np.random.rand(1,2)
+    H[0,0] = vals_V_target[0][i]
+    H[0,1] = vals_V_target[1][i]
 
     for j in range(len(G)):
-        M[0, j] = M[0, j] + dt*G[j](V, M, V_half)
+        M[0, j] = M[0, j] + dt*G[j](H, M, V_half)
         vals_M[j].append(M[0, j])
 
     for j in range(len(F)):
-        V[0,j] = V[0,j] + dt*F[j](V, M, W_inh, V_half)
+        V[0,j] = H[0,j] + dt*F[j](H, M, W_inh, V_half) # H instead of V
         vals_V[j].append(V[0,j])
 
     for j in range(Z_inh.shape[0]):
         for k in range(Z_inh.shape[1]):
-            Z_inh[j,k] = Z_inh[j,k] + dt*RHS_Z_inh[j,k](V, M, W_inh, Z_inh, Y_inh, V_half)
+            Z_inh[j,k] = Z_inh[j,k] + dt*RHS_Z_inh[j,k](H, M, W_inh, Z_inh, Y_inh, V_half)
             vals_Z_inh[j,k].append(Z_inh[j,k])
 
     for j in range(Y_inh.shape[0]):
         for k in range(Y_inh.shape[1]):
-            Y_inh[j,k] = Y_inh[j,k] + dt*RHS_Y_inh[j,k](V, M, W_inh, Z_inh, Y_inh, V_half)
+            Y_inh[j,k] = Y_inh[j,k] + dt*RHS_Y_inh[j,k](H, M, W_inh, Z_inh, Y_inh, V_half)
             vals_Y_inh[j,k].append(Y_inh[j,k])
 
-    H = np.random.rand(1,2)
-    H[0,0] = vals_V_target[0][i%(int(t_target[-1]/dt)-1)]
-    H[0,1] = vals_V_target[1][i%(int(t_target[-1]/dt)-1)]
-    etha = 0.5*1e-7
+    etha = 2*1e-7
 
-    if i > int(10000/dt): #for the network to settle into a limit cycle
+    if i > int(5000/dt): #for the network to settle into a limit cycle
         for j in range(W_inh.shape[1]):
-            if j == 1 : #changing only one of parameters
-                W_inh[0, j] = W_inh[0, j] + etha*dt*RHS_W_inh[j](V, H, Z_inh, Y_inh)
-                vals_W_inh[0, j].append(W_inh[0, j])
+            W_inh[0, j] = W_inh[0, j] + etha*dt*RHS_W_inh[j](V, H, Z_inh, Y_inh)
+            vals_W_inh[0, j].append(W_inh[0, j])
     else:
         for j in range(W_inh.shape[1]):
-            if j == 1 : #changing only one of parameters
-                W_inh[0, j] = W_inh[0, j]
-                vals_W_inh[0, j].append(W_inh[0, j])
+            W_inh[0, j] = W_inh[0, j]
+            vals_W_inh[0, j].append(W_inh[0, j])
 
-    errors.append((H[0,0] - V[0,0])**2 + (H[0,1] - V[0,1])**2)
-    error = np.mean(list(errors))
     t.append(t[-1] + dt)
-    i += 1
 
 # PLOTTING THE RESULTS
 fr1 = 1/(1+np.exp(-(np.array(vals_V[0]) - V_half)/k_v))
@@ -230,8 +205,10 @@ plt.plot(t[start:], fr2[start:], "r-", linewidth = 3, alpha = 0.7)
 plt.grid(True)
 plt.show()
 
-print(W_inh)
 fig2 = plt.figure(figsize=(10,4))
-plt.plot(t,vals_W_inh[0, 1], "k--", linewidth = 3, alpha = 0.7)
+styles = ["k","b","r","g"]
+for i in range(4):
+    plt.plot(t,vals_W_inh[0, i], styles[i], linewidth = 3, alpha = 0.7)
 plt.grid(True)
 plt.show()
+print(W_inh)
